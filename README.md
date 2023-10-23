efaa2ef48946057745e93730c1db67ad33817a16
BarchamMal <115943779+BarchamMal@users.noreply.github.com>
2023-10-23 10:34:09 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# mikes-miscellanea
+ A mod with all the things that didn'n fit into my other mods.
---
+++ src/client/java/net/barch/mm/MikesMiscellaneaClient.java
@@ -0,0 +1,10 @@
+package net.barch.mm;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MikesMiscellaneaClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
